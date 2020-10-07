26314d0fd683d25b046defdd1ab3c88d106d042d
Zahidul Hasan <zahid.xd@gmail.com>
2020-10-07 02:02:30 -0500

    Added unit tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/*
+This file is required to prevent the following error:
+
+Mongoose: looks like you're trying to test a Mongoose app with Jest's default jsdom test environment.
+Please make sure you read Mongoose's docs on configuring Jest to test Node.js apps: http://mongoosejs.com/docs/jest.html
+
+Check it here
+
+https://jestjs.io/docs/en/configuration
+https://stackoverflow.com/questions/59223433/jest-error-mongoose-looks-like-youre-trying-to-test-a-mongoose-app-with-jest
+
+*/
+
+// module.exports = {
+//   testEnvironment: 'node'
+// };
